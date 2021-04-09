b2567837b62d2fc7da9c00c510042168440ad035
hansmohob <36814586+hansmohob@users.noreply.github.com>
2021-04-09 12:53:07 +0100

    Create ConfigureWindowsForAnsible.ps1

---
+++ scripts/ConfigureWindowsForAnsible.ps1
@@ -0,0 +1,6 @@
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -DisableBasicAuth -EnableCredSSP
+winrm get winrm/config/Service
+winrm get winrm/config/Winrs
