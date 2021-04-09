512a44c03a5b4749358054ed0a602b3e5d72b4fd
hansmohob <36814586+hansmohob@users.noreply.github.com>
2021-04-09 13:35:41 +0100

    Update ConfigureRemotingForAnsible.ps1

--- scripts/ConfigureRemotingForAnsible.ps1
+++ scripts/ConfigureRemotingForAnsible.ps1
@@ -451,3 +451,12 @@ Else
     Throw "Unable to establish an HTTP or HTTPS remoting session."
 }
 Write-VerboseLog "PS Remoting has been successfully configured for Ansible."
+
+# Mo added the following
+
+$url = "https://raw.githubusercontent.com/ansible/ansible/devel/examples/scripts/ConfigureRemotingForAnsible.ps1"
+$file = "$env:temp\ConfigureRemotingForAnsible.ps1"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url, $file)
+powershell.exe -ExecutionPolicy ByPass -File $file -DisableBasicAuth -EnableCredSSP
+winrm get winrm/config/Service
+winrm get winrm/config/Winrs
