fe9eccdbd7d217340d5fc21e4d8be6218b1f7034
hansmohob <36814586+hansmohob@users.noreply.github.com>
2021-06-02 06:55:39 +0100

    Update ubuntu-setup-ansible.sh

--- scripts/ubuntu-setup-ansible.sh
+++ scripts/ubuntu-setup-ansible.sh
@@ -7,14 +7,14 @@
 # sudo apt autoremove --assume-yes
 
 # Install azcli
-# curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
+curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 
 # Install ansible and dependencies
 # sudo apt update
 # sudo apt install software-properties-common
 # sudo apt-add-repository --yes --update ppa:ansible/ansible
 # sudo apt install ansible --assume-yes
-# sudo apt install python3-pip --assume-yes
+
 ## sudo pip3 install pywinrm
 ## sudo ansible-galaxy collection install ansible.windows
 ## sudo ansible-galaxy collection install community.windows
@@ -26,22 +26,22 @@
 #curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 #python3 get-pip.py --user
 #python3 -m pip install --user ansible
-#sudo apt install software-properties-common
-#sudo add-apt-repository --yes --update ppa:ansible/ansible
-#sudo apt install ansible --assume-yes
-#sudo apt install python3-pip --assume-yes
-#sudo pip3 install --upgrade pip
-#ansible-galaxy collection install azure.azcollection
-#wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
-#sudo pip3 install -r requirements-azure.txt
+
+sudo apt install software-properties-common --assume-yes
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt install ansible --assume-yes
+sudo ansible-galaxy collection install azure.azcollection
+wget https://raw.githubusercontent.com/ansible-collections/azure/dev/requirements-azure.txt
+sudo apt install python3-pip --assume-yes
+sudo pip3 install -r requirements-azure.txt
 
 #create folder structure and secure
-#sudo groupadd grpsecansadmin
-#usermod -a -G grpsecansadmin localadmin
-#usermod -a -G grpsecansadmin root
-#sudo mkdir /etc/ansible/group_vars
-#sudo chgrp grpsecansadmin /etc/ansible/group_vars
-#sudo chmod g+rwx /etc/ansible/group_vars
-#sudo chgrp grpsecansadmin /etc/ansible
-#sudo chmod g+rwx /etc/ansible
-#sudo mv etc/ansible/ansible.cfg etc/ansible/ansible.cfgold
+sudo groupadd grpsecansadmin
+sudo usermod -a -G grpsecansadmin localadmin
+sudo usermod -a -G grpsecansadmin root
+sudo mkdir /etc/ansible/group_vars
+sudo chgrp grpsecansadmin /etc/ansible/group_vars
+sudo chmod g+rwx /etc/ansible/group_vars
+sudo chgrp grpsecansadmin /etc/ansible
+sudo chmod g+rwx /etc/ansible
+sudo mv /etc/ansible/ansible.cfg /etc/ansible/ansible.cfgold
